ac9b57fd0c77f14bc1335f0ed54e49895216bca5
Ye_meng <133776733+Yemengrr@users.noreply.github.com>
2023-08-21 15:37:07 +0800

    Add files via upload

---
+++ .github/_20230323_2.java
@@ -0,0 +1,71 @@
+package _20230323;
+
+/**
+ * 打印图形
+ */
+public class _20230323_2 {
+    public static void main(String[] args) {
+        /**
+         * 打印直角三角形（左下）
+         */
+//        for(int i=1;i<=5;i++){
+//            for(int j=i;j!=0;j--)
+//            System.out.print("*");
+//            System.out.println("");
+//        }
+        /**
+         * 打印直角三角形（右下）
+         */
+//        for(int i=1;i<=5;i++){
+//            for(int j=5-i;j!=0;j--){
+//                System.out.print(" ");
+//            }
+//            for(int k=i;k!=0;k--){
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+        /**
+         * 打印直角三角形（左上）
+         */
+//        for(int i=5;i>=0;i--){
+//            for(int k=i;k!=0;k--){
+//                System.out.print("*");
+//            }
+//            for(int j=5-i;j!=0;j--){
+//                System.out.print(" ");
+//            }
+//            System.out.println("");
+//        }
+        /**
+         * 打印直角三角形（右上）
+         */
+//        for(int i=5;i>=0;i--){
+//            for(int j=5-i;j!=0;j--){
+//                System.out.print(" ");
+//            }
+//            for(int k=i;k!=0;k--){
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+        /**
+         * 打印正三角形
+         */
+//        for(int i=1;i<=5;i++){
+//            for(int j=5-i;j!=0;j--){
+//                System.out.print(" ");
+//            }
+//            for(int k=i;k!=0;k--){
+//                System.out.print("*");
+//            }
+//            for(int k=i;k!=1;k--){
+//                System.out.print("*");
+//            }
+//            for(int j=5-i;j!=0;j--){
+//                System.out.print(" ");
+//            }
+//            System.out.println("");
+//        }
+    }
+}
